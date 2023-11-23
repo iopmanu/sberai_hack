016ce64db6e9a59756d625b6dea776e924c7be72
iopmanu <vanya-doronin@bk.ru>
2023-11-23 16:36:50 +0300

    application: added model config

--- application/application.py
+++ application/application.py
@@ -11,6 +11,7 @@ sys.path.append(os.getcwd() + '/../model')
 
 
 from configs.configs import MODEL_NAME
+from configs_app import MODEL_LOGGING, MODEL_TEMPERATURE, MODEL_MAX_NEW_TOKENS
 from TableFormer import TableFormer
 
 app = FastAPI()
@@ -19,13 +20,10 @@ former = TableFormer(MODEL_NAME)
 
 @app.post("/create_questions/")
 async def create_questions(
-        image_file,
-        logging: bool = False,
-        temperature: float = 0.2,
-        max_new_tokens: int = 512
+        image_file: str,
 ):
-    result = await former.create_questions(image_file=image_file, logging=logging,
-                                           temperature=temperature, max_new_tokens=max_new_tokens)
+    result = await former.create_questions(image_file=image_file, logging=MODEL_LOGGING,
+                                           temperature=MODEL_TEMPERATURE, max_new_tokens=MODEL_MAX_NEW_TOKENS)
     return {"questions": result}
 
 
@@ -33,10 +31,8 @@ async def create_questions(
 async def create_vqa(
         image_file,
         question: str,
-        logging: bool = False,
-        temperature: float = 0.2,
-        max_new_tokens: int = 512
+
 ):
-    result = await former.predict(image_file=image_file, input=question, logging=logging,
-                                  temperature=temperature, max_new_tokens=max_new_tokens)
+    result = await former.predict(image_file=image_file, input=question, logging=MODEL_LOGGING,
+                                  temperature=MODEL_TEMPERATURE, max_new_tokens=MODEL_MAX_NEW_TOKENS)
     return {"question": question, "answer": result}
