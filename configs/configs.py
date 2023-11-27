260937f8ac79a789978abf219d1b40583d96eb3b
iopmanu <vanya-doronin@bk.ru>
2023-11-27 20:51:03 +0300

    configs and models: minor fixes

--- configs/configs.py
+++ configs/configs.py
@@ -6,7 +6,11 @@ DEFAULT_IMAGE_VALIDATION = DEFAULT_IMAGE_QUANTITY_DELIMETER * 2
 
 NUM_SECONDS_TO_SLEEP = 3
 
-LLAVA_ENDPOINTS = ['http://localhost:8000']
+LLAVA_URLS = ['http://localhost:8000']
+FAISS_APPLICATION_URL = ['http://localhost:8080']
 
+EVALUATION_ON = True
 GPT_EVALUATION_QUESTIONS_PROMPT = 'We would like to request your feedback on the perfomance of our AI assistant in responce of simmilarity in meaning of common theme of these questions.\n Please rate simmilarity with an overall score on a scale of 1 to 10, where higher score indicates better overall relevance.\n Please output a single line containing only one value indicating a score.'
 GPT_EVALUATION_QUESTIONS_RELEVANCE_PROMPT = 'We would like to request your feedback on the perfomance of our AI assistant in responce of relevance of generated question to picture description.\n Please rate relevance with an overall score on a scale of 1 to 10, where higher score indicates better overall relevance.\n Please output a single line containing only one value indicating a score.'
+GPT_EVALUATION_VQA_PROMPT = 'We would like to request your feedback on the perfomance of our AI assistant in responce of relevance answer to the given question.\n Please rate relevance with an overall score on a scale of 1 to 10, where higher score indicates better overall relevance.\n Please output a single line containing only one value indicating a score.'
+
--- model/columns_finder.py
+++ model/columns_finder.py
@@ -53,7 +53,7 @@ class ColumnsFinder:
     async def create_faiss_questions(self, image_files: List[str]):
         images_minibatch = list(np.random.choice(
             image_files,
-            int(np.ceil(len(image_files) // DEFAULT_IMAGE_QUANTITY_DELIMETER, 1))
+            size=max(len(image_files) // DEFAULT_IMAGE_QUANTITY_DELIMETER, 1)
         ))
         await self.__fill_questions_stash(images_minibatch, self.questions_stash)
 
@@ -74,7 +74,7 @@ class ColumnsFinder:
 
         validation_images = np.random.choice(
             validation_qeustions := [question for question in image_files if question not in images_minibatch],
-            int(np.ceil(len(validation_qeustions) // DEFAULT_IMAGE_VALIDATION))
+            size=max(len(validation_qeustions) // DEFAULT_IMAGE_VALIDATION, 1)
         )
         validation_questions = []
         await self.__fill_questions_stash(validation_images, validation_questions)
