2578f933a9d060ab450c898e5d5c41f51b8e1694
Mahh <the_mahh@mail.ru>
2023-11-29 21:46:20 +0300

    add video2pic and opencv

---
+++ video2pics/video2pics.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+import pathlib
+import shutil
+
+PROJECT_ROOT = pathlib.Path(__file__).parent.parent
+INPUT_DIR = PROJECT_ROOT / "./video/"
+OUTPUT_DIR = PROJECT_ROOT / "./output_pics/"
+TIME_SKIPS = float(1000) # 1000 - 1 second
+
+
+def video2pics(filename):
+    if os.path.exists(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
+    vidcap = cv2.VideoCapture(str(INPUT_DIR) + f'/{str(filename)}')
+    count = 0
+    while vidcap.isOpened():
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * TIME_SKIPS))
+        sucess, image = vidcap.read()
+        if sucess:
+            cv2.imwrite(os.path.join(OUTPUT_DIR, '%d.png') % count, image)
+            count += 1
+        else:
+            break
+    cv2.destroyAllWindows()
+    vidcap.release()
